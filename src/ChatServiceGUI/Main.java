05e8d374a219b6a0d3092d6aac80fc32f7bd2fb4
Justin Fernández Badilla <justinfernandez@estudiantec.cr>
2021-08-26 23:17:28 -0600

    Javadoc First Documentation

--- src/ChatServiceGUI/Main.java
+++ src/ChatServiceGUI/Main.java
@@ -7,22 +7,38 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class Main extends Application {
-
+    /**
+     * The Main Java Class that creates two windows which have the same
+     * design.
+     *
+     * @author Justin Fernández
+     * @version 1
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /**
+         *
+         */
+        // Creates other stage
         Stage secondStage = new Stage();
+
         Parent root = FXMLLoader.load(getClass().getResource("ChatServiceApp.fxml"));
         primaryStage.setTitle("Chat Service");
+        // Adjust the dimensions of the window.
         primaryStage.setScene(new Scene(root, 500, 400));
+        // Adjust the window or desktop position to appear the window
         primaryStage.setX(200);
         primaryStage.setY(200);
+        // Avoid to change the initial dimmensions of a window.
         primaryStage.setResizable(false);
         Image icon = new Image(getClass().getResourceAsStream("Images/Icon.png"));
+        // Adds a personalize icon for the window
         primaryStage.getIcons().add(icon);
         primaryStage.show();
+
+        // Code for the second window, it is the same, only different initial postion.
         Parent root2 = FXMLLoader.load(getClass().getResource("ChatServiceApp.fxml"));
         secondStage.setTitle("Chat Service");
         secondStage.setScene(new Scene(root2, 500, 400));
--- src/ChatServiceGUI/Server.java
+++ src/ChatServiceGUI/Server.java
@@ -1,21 +1,42 @@
 package ChatServiceGUI;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
+    /**
+     * Create the server which the users will connect to interchange
+     * information among them.
+     *
+     * @author Justin Fernández
+     * @version 1
+     */
     private ArrayList<ClientHandler> clientela;
     private ServerSocket serverSystem;
 
     public Server() {
+        /**
+         * Creates an ArrayList of ClientHandler that will allow to
+         * choose which client send the information.
+         *
+         * @param clientela this is the ArrayList of ClientHandler
+         */
         clientela = new ArrayList<ClientHandler>();
     }
 
     public void ServerCreate() throws IOException {
+        /**
+         * Creates the Server and waits for clients to connect.
+         * Adds the connected clients to ClientHandler object to manage easy, also
+         * start the thread of ClientHandler.
+         *
+         * @param serverSystem this is the ServerSocket that creates the system for using later.
+         * @param client this is the return value when the server accept a new user.
+         * @param ClientHandler this is a object that gestionate the connected clients from a server.
+         */
         final int PORT = 5000;
         serverSystem = new ServerSocket(PORT);
 
@@ -29,10 +50,17 @@ public class Server {
     }
 
     public void kill() throws IOException {
+        /**
+         * Closes or kills the created server system.
+         */
         serverSystem.close();
     }
 
     public static void main(String[] args) {
+        /**
+         * Main method of the Server Class that start running when the program is executed.
+         * @param myServer this is the instance of the class.
+         */
         Server myServer = new Server();
         try {
             myServer.ServerCreate();
