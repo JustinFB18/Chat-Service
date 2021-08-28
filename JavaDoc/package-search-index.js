4d5009a59ed6dbf910d3a0536da599fe8929e54d
Justin Fernández Badilla <justinfernandez@estudiantec.cr>
2021-08-27 21:59:20 -0600

    Javadoc First Documentation Documents restructuring 2

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ChatServiceGUI"},{"l":"ChatServiceGUI.ChatClient"},{"l":"ChatServiceGUI.ChatServer"}];updateSearchResults();
